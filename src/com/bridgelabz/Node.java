99c926ced6ce1cc55a83ce7fd82c719adfbf33c3
sravan <sravan.k.vodnala@gmail.com>
2022-06-23 18:47:18 +0530

    Added: Finding node in the tree

--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -1,30 +1,32 @@
 package com.bridgelabz;
 
-    public class Node<T> {
-        Node next;
-        T data;
 
-        public Node(T data) {
-            this.data = data;
-        }
+public class Node<T> {
+    Node next;
+    T data;
 
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
-        }
-        public void display(){
-            System.out.print(data+" -->");
-        }
+    public Node(T data) {
+        this.data = data;
     }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void display() {
+        System.out.print(data + " -->");
+    }
+}
 
