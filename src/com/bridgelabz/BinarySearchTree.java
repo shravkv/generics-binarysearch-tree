7af89b2f2aee22f7d3cf420ad7a1b5e5f20da1d2
shravkv <104706744+shravkv@users.noreply.github.com>
2022-06-25 08:03:17 +0530

    Update BinarySearchTree.java

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -1,47 +1,63 @@
 package com.bridgelabz;
 
 public class BinarySearchTree {
-    Node root;
+    static Node root;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Binary Search Tree program ");
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
-        binarySearchTree.insertionElements(56);
-        binarySearchTree.insertionElements(30);
-        binarySearchTree.insertionElements(70);
-        binarySearchTree.sorting();
-        binarySearchTree.toPrint();
+        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+        binarySearchTree.add(56);
+        binarySearchTree.add(30);
+        binarySearchTree.add(70);
+        System.out.println();
+        System.out.println("InOrder Print is left-->root-->right:");
+        binarySearchTree.inOrderPrint(root);
+        System.out.println();
+        System.out.println("PreOrder Print is root-->left-->right:");
+        binarySearchTree.preOrderPrint(root);
+        System.out.println();
+        System.out.println("PostOrder Print is left-->right-->root:");
+        binarySearchTree.postOrderPrint(root);
     }
 
-    public <T> void insertionElements(T data) {
-        Node node = new Node(data);
-        node.setData(data);
-        node.setNext(root);
-        root = node;
-    }
-
-    public <T> void sorting() {
-        Node currentNode = root, index;
-        while (currentNode != null) {
-            index = currentNode.getNext();
-            while (index != null) {
-                if ((int) currentNode.getData() > (int) index.getData()) {
-                    int temp = (int) currentNode.getData();
-                    currentNode.setData(index.getData());
-                    index.setData(temp);
-                }
-                index = index.getNext();
-            }
-            currentNode = currentNode.getNext();
+    public <T> void insertElements(Node<T> root, T key) {
+        if (root == null) {
+            root = new Node<>(key);
         }
-
+        if (key.compareTo(root.key) < 0) {
+            root.left = insertElements(root.left, key);
+        } else if (key.compareTo(root.key) > 0) {
+            root.right = insertElements(root.right, key);
+        }
+        return root;
     }
 
-    public void toPrint() {
-        Node currentNode = root;
-        while (currentNode != null) {
-            currentNode.display();
-            currentNode = currentNode.next;
+    public void add(T key) {
+        root = insertElements(root, key);
+    }
+
+    public void inOrderPrint(Node<T> node) {
+        if (node != null) {
+            inOrderPrint(node.left);
+            System.out.print(node.key + " -->");
+            inOrderPrint(node.right);
+
+        }
+    }
+
+    public void preOrderPrint(Node<T> node) {
+        if (node != null) {
+            System.out.print(node.key + "-->");
+            preOrderPrint(node.left);
+            preOrderPrint(node.right);
+        }
+    }
+
+    public void postOrderPrint(Node<T> node) {
+        if (node != null) {
+            preOrderPrint(node.left);
+            preOrderPrint(node.right);
+            System.out.print(node.key + "-->");
         }
     }
 }
